2150fa8355539e10cc3b3750033577c190696950
Dmitry Vengerov <hardbeat34@gmail.com>
2020-06-01 22:35:22 +0300

    add factory

---
+++ factory/factory.js
@@ -0,0 +1,47 @@
+class TailFactory {
+    constructor(props) {
+        this.tailLenght = props.tailLength;
+    }
+};
+
+class TorsoFactory {
+    constructor() {
+        this.color = props.color;
+    }
+};
+
+class HeadFactory {
+    constuctor(props) {
+        this.snoutLenth = props.snotLenth;
+    }
+};
+
+class ReptilePartFactory {
+    consructor(type, props) {
+        if(type === 'tail') {
+            return new TailFactory(props);
+        }
+        if(type === 'torso') {
+            return new TorsoFactory(props);
+        }
+        if(type === 'head') {
+            return new HeadFactory(props);
+        }
+        /** 
+         *
+         *
+         **/
+    }
+};
+
+
+let alligator = {};
+let alligatorProps = {
+    tailLength : 2.5,
+    color: "green",
+    shoutLenth: 1
+};
+
+alligator.tail  = new ReptilePartFactory('tail',  alligatorProps);
+alligator.torso = new ReptilePartFactory('torso', alligatorProps);
+alligator.head  = new ReptilePartFactory('head',  alligatorProps);
